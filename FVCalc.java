ba640be48a4f43c1b0983b7a1a25ffe05cd80ea9
TziporaFeuchtwanger <tzipi2908@gmail.com>
2024-11-14 20:50:30 +0200

    home ork added

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,12 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+int sum = Integer.parseInt(args[0]);
+double rate = Double.parseDouble(args[1])/100;
+int years = Integer.parseInt(args[2]);
+int future = (int)((sum)*(Math.pow(1 + rate, years)));
+Math.ceil(rate);
+System.out.println("After "+years+" years, "+sum+"$ "+"saved at "+(double)(rate*100)+"% "+"will yield $"+future);
 	}
 }
+//After 2 years, $100 saved at 10.0% will yield $121
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,33 @@
-// Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String args[]) {
+        String n = args[0];
+        
+        int length = n.length();
+        
+    
+        if (length == 3) {
+            System.out.print(n.charAt(0) + " hundreds, ");
+            System.out.print(n.charAt(1) + " tens, and ");
+            System.out.print(n.charAt(2) + " ones.");
+        } 
+        
+        else if (length == 2) {
+            System.out.print("0 hundreds, ");
+            System.out.print(n.charAt(0) + " tens, and ");
+            System.out.print(n.charAt(1) + " ones.");
+        } 
+      
+        else if (length == 1) {
+            System.out.print("0 hundreds, 0 tens, and ");
+            System.out.print(n.charAt(0) + " ones.");
+        } 
+       
+        else {
+            System.out.print("Invalid input.");
+        }
+    }
 }
+
+
+
+	
