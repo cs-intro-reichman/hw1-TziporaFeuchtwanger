3635cd5b710f44ab0ff08415fef1de7c889a52fc
TziporaFeuchtwanger <tzipi2908@gmail.com>
2024-11-14 14:04:50 +0200

    updated Bill3

--- Bill3.java
+++ Bill3.java
@@ -4,10 +4,10 @@ public class Bill3 {
 		String name0 = args[0];
 		String name1 = args[1];
 		String name2 = args[2];
-        int sum = Integer.parseInt(args[3]);
+        double sum = Double.parseDouble(args[3]);
 		
-		double bill = sum/3;
-		Math.ceil(bill);
-		System.out.print("dear "+name0+" "+name1+" "+name2+" pay "+bill+" shekel each");
+		double bill = Math.ceil(sum/3);
+		System.out.print("Dear "+name2+", "+name1+", "+name0+": pay "+bill+" shekel each.");
+		//Dear Dan, Lisa, and Ron: pay 34.0 Shekels each.
 	}
 }
